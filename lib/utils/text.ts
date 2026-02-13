44d658ab1a2b9e01c608edc20c90233749df1358
Jakub Pniewski <jakub.pniewski@icloud.com>
2026-02-13 13:06:46 +0100

    fix hardspaces

--- lib/utils/text.ts
+++ lib/utils/text.ts
@@ -1,24 +1,18 @@
 import { HARD_SPACE_WORDS } from "@constants";
 
-
 export function applyHardSpaceBreaks(
     text: string,
     hardSpaceWords: string[] = HARD_SPACE_WORDS
 ): string {
     const nbsp = "\u00A0";
     const words = text.split(" ");
-    let targetWordIndex: number = -2;
     const processedWords = words.map((word, i) => {
-        if (hardSpaceWords.includes(word)) {
-            targetWordIndex = i;
-            return "";
+        const isCurrTargetWord = hardSpaceWords.includes(word);
+        if (i === words.length) return word;
+        if (isCurrTargetWord) {
+            return word + nbsp;
         }
-        if (targetWordIndex === i - 1) {
-            const bindedWords = words[targetWordIndex] + nbsp + word;
-            targetWordIndex = -1;
-            return bindedWords;
-        }
-        return word;
+        return word + " ";
     });
-    return processedWords.join(" ");
+    return processedWords.join("");
 }
