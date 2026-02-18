29c57580609501f0ff7678ac448bd067d5be93af
Jakub Pniewski <jakub.pniewski@icloud.com>
2026-02-18 14:16:20 +0100

    small fix

--- components/layout/group.tsx
+++
@@ -1,8 +0,0 @@
-interface GroupProps<T> {
-    items: T[];
-    readonly render: (item: T, index: number) => React.ReactNode;
-}
-
-export default function Group<T>({ items, render }: GroupProps<T>) {
-    return <>{items.map(render)}</>;
-}
