abbfb726d32d175ae0870008373141fbd79dc6cb
NeoNeqs <76450252+NeoNeqs@users.noreply.github.com>
2026-03-01 20:53:03 +0100

    Test

--- components/pages/HomePage.tsx
+++ components/pages/HomePage.tsx
@@ -5,18 +5,18 @@ import { HomeHero } from "../sections/HomeHero";
 import { Separator } from "../ui/separator";
 import dynamic from "next/dynamic";
 
-const HomeStart = dynamic(() => import("../sections/HomeStart"), { ssr: false });
+const HomeStart = dynamic(() => import("../sections/HomeStart"));
 const HomeShowcase = dynamic(() =>
-    import("../sections/HomeShowcase").then((m) => ({ default: m.HomeShowcase })), { ssr: false }
+    import("../sections/HomeShowcase").then((m) => ({ default: m.HomeShowcase }))
 );
 const HomePackages = dynamic(() =>
-    import("../sections/HomePackages").then((m) => ({ default: m.HomePackages })), { ssr: false }
+    import("../sections/HomePackages").then((m) => ({ default: m.HomePackages }))
 );
 const HomeSolutions = dynamic(() =>
-    import("../sections/HomeSolutions").then((m) => ({ default: m.HomeSolutions })), { ssr: false }
+    import("../sections/HomeSolutions").then((m) => ({ default: m.HomeSolutions }))
 );
 const HomeCtaFooter = dynamic(() =>
-    import("../sections/HomeCtaFooter").then((m) => ({ default: m.HomeCtaFooter })), { ssr: false }
+    import("../sections/HomeCtaFooter").then((m) => ({ default: m.HomeCtaFooter }))
 );
 
 export default function HomePage() {
